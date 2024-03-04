643c85078d2a2c12367ac6d24c031af9a052c82a
BrunoLima-dev <brunoprograme04@gmail.com>
2024-03-04 17:12:42 -0300

    Atualiza token da API do Boleto Simples para utilizar variável de ambiente

--- config/initializers/boleto_simples.rb
+++ config/initializers/boleto_simples.rb
@@ -4,7 +4,7 @@ BoletoSimples.configure do |c|
   c.environment = :sandbox # defaut :sandbox
   # production - https://app.kobana.com.br/conta/api/tokens
   # sandbox - https://app-sandbox.kobana.com.br/conta/api/tokens
-  c.api_token = 'W--kUZC3d0OkDLYSuxhvBJVUTqX9ms3MAnCYE_Qe7Mw'
+  c.api_token = ENV['BOLETOSIMPLES_API_TOKEN']
   c.user_agent = 'brunolima.dev703@gmail.com' #Colocar um e-mail válido para contatos técnicos relacionado ao uso da API.
   c.debug = true
   c.custom_headers = { 'X-CUSTOM' => 'CONTENT' }
